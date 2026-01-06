c7d276ddbf5afc355b8a81035d65ab6b7af409b8
Julian Cable <julian.cable@yahoo.com>
2026-01-06 09:31:47 +0000

    wip

--- ports/hamlib/portfile.cmake
+++ ports/hamlib/portfile.cmake
@@ -5,3 +5,6 @@ vcpkg_from_github(
     SHA512 8a6bd56cc1f9685a744eda8b88efc068e2f94d3e
     HEAD_REF master
 )
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
+vcpkg_cmake_install()
+vcpkg_install_copyright("${SOURCE_PATH}/COPYING")
