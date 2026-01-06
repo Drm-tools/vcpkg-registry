e77b61ae6a3cde9fea8c0e15d8f29981ee7a26f4
Julian Cable <julian.cable@yahoo.com>
2026-01-06 20:03:19 +0000

    sha

--- ports/hamlib/portfile.cmake
+++ ports/hamlib/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Hamlib/Hamlib
     REF 4.6.5
-    SHA512 0
+    SHA512 02e626866fa1dba63cc83923938d6632d54bdb10fea1970110fa59f0dbf06c1490142de30b66f4a00fa98cee49eda271b3bbdc114c8a753977655544dccfdb66
     HEAD_REF master
 )
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
